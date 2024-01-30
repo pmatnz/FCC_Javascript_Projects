29268f1d191c6a8b8d84474e8dbdaeb70b02c652
pmatnz <paullmat@gmail.com>
2024-01-30 21:07:11 +1300

    project complete

--- jsprojects_calorie_counter.js
+++ jsprojects_calorie_counter.js
@@ -1,40 +1,119 @@
-//Access to HTML elements
 const calorieCounter = document.getElementById("calorie-counter");
 const budgetNumberInput = document.getElementById("budget");
 const entryDropdown = document.getElementById("entry-dropdown");
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
-
-//Global variables
 let isError = false;
 
-//Functions
 function cleanInputString(str) {
-	const regex = /[\+\-\s]/g;
+	const regex = /[+-\s]/g;
 	return str.replace(regex, "");
 }
 
 function isInvalidInput(str) {
-	let regex = /\de\d/i; // e is the exponant symbol, + ensures a match one or more. shorthand uses \
-	// let regex = /[0-9]e[0-9]/i; - this is a longer way without using shorthand \d
+	const regex = /\d+e\d+/i;
 	return str.match(regex);
 }
 
 function addEntry() {
-	let targetInputContainer = document.querySelector(
+	const targetInputContainer = document.querySelector(
 		`#${entryDropdown.value} .input-container`
 	);
-	// Gets the value of the users input via value property
-	let entryNumber =
-		targetInputContainer.querySelectorAll('input[type="text"').length + 1; // can select all elements on a type on a page
+	const entryNumber =
+		targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+	const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+	targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
 
-	let HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name"> Entry ${entryNumber} Name </label> 
-	<input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}}
-	<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-	<input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">`; // value property reresents the text entered by the user
+function calculateCalories(e) {
+	e.preventDefault();
+	isError = false;
 
-	targetInputContainer.innerHTML += HTMLString; // using innerHTML and += this will add my HTMLString to the current HTML page
+	const breakfastNumberInputs = document.querySelectorAll(
+		"#breakfast input[type=number]"
+	);
+	const lunchNumberInputs = document.querySelectorAll(
+		"#lunch input[type=number]"
+	);
+	const dinnerNumberInputs = document.querySelectorAll(
+		"#dinner input[type=number]"
+	);
+	const snacksNumberInputs = document.querySelectorAll(
+		"#snacks input[type=number]"
+	);
+	const exerciseNumberInputs = document.querySelectorAll(
+		"#exercise input[type=number]"
+	);
+
+	const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+	const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+	const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+	const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+	const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+	const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+	if (isError) {
+		return;
+	}
+
+	const consumedCalories =
+		breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+	const remainingCalories =
+		budgetCalories - consumedCalories + exerciseCalories;
+	const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+	output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(
+		remainingCalories
+	)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>
+  <p>${consumedCalories} Calories Consumed</p>
+  <p>${exerciseCalories} Calories Burned</p>
+  `;
+
+	output.classList.remove("hide");
+}
+
+function getCaloriesFromInputs(list) {
+	let calories = 0;
+
+	for (let i = 0; i < list.length; i++) {
+		const currVal = cleanInputString(list[i].value);
+		const invalidInputMatch = isInvalidInput(currVal);
+
+		if (invalidInputMatch) {
+			alert(`Invalid Input: ${invalidInputMatch[0]}`);
+			isError = true;
+			return null;
+		}
+		calories += Number(currVal);
+	}
+	return calories;
+}
+
+function clearForm() {
+	let inputContainers = Array.from(
+		document.querySelectorAll(".input-container") // DON"T FORGET THE . IT'S A CLASS!!!!!
+	); // this returns a Nodelist (list of elements)
+
+	for (let i = 0; i < inputContainers.length; i++) {
+		inputContainers[i].innerHTML = ""; // This will clear all content on the form and set to an empty string
+	}
+	budgetNumberInput.value = "";
+	output.innerText = "";
+	output.classList.add("hide");
 }
 
 addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click", clearForm);
