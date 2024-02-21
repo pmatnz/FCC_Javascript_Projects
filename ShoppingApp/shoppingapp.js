e89570f6f12e9b8323f2db8d031a19ad4b7dbe55
pmatnz <44563124+pmatnz@users.noreply.github.com>
2024-02-21 21:54:00 +1300

    Shopping app done

--- ShoppingApp/shoppingapp.js
+++ ShoppingApp/shoppingapp.js
@@ -2,7 +2,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"; // importing database firebase
 // prettier-ignore
-import { getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"; // import ref (reference) and push which will push to database
+import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"; // import ref (reference) and push which will push to database
 
 const appSettings = {
 	databaseURL:
@@ -21,13 +21,27 @@ const shoppingList = document.getElementById("shopping-list");
 
 //calling onValue to get data from the BD, also using snapshot function
 onValue(shoppingInDB, function (snapshot) {
-	clearShoppingList();
-	//console.log(snapshot.val()); // use snapshot which gets the current data . val
-	const shoppingArray = Object.values(snapshot.val()); // converting the snapshot object into an array
-	for (let i = 0; i < shoppingArray.length; i++) {
-		appendItemToList(shoppingArray[i]);
+	//let itemsArray = Object.entries(snapshot.val());
+
+	if (snapshot.exists()) {
+		// DON"T FORGET THE () FOR THE METHOD!!!!!!!!!!!!
+		// using this will run the below code if it's true
+		clearShoppingList();
+		//console.log(snapshot.val()); // use snapshot which gets the current data . val
+		let shoppingArray = Object.entries(snapshot.val()); // converting the snapshot object into an array
+		for (let i = 0; i < shoppingArray.length; i++) {
+			let currentItem = shoppingArray[i]; // not sure what purpose this serves yet ??????????????
+			let currentItemID = currentItem[0]; // not sure what purpose this serves yet ??????????????
+			let currentItemValue = currentItem[1]; // not sure what purpose this serves yet ??????????????
+
+			appendItemToList(shoppingArray[i]);
+		}
+	} else {
+		shoppingList.innerHTML = "No items in cart";
 	}
 });
+
+//*****************EVENT LISTENERS*************************//
 
 addButton.addEventListener("click", function () {
 	let inputValue = textInput.value;
@@ -36,14 +50,28 @@ addButton.addEventListener("click", function () {
 	clearInputTextField();
 });
 
+//*****************FUNCTIONS******************//
+
 function clearInputTextField() {
 	// Clears the text field after item has been added to list
 	textInput.value = "";
 }
 
-function appendItemToList(inputValue) {
+function appendItemToList(item) {
 	// apends user input into HTML list
-	shoppingList.innerHTML += `<li>${inputValue}</li>`; // Adds input to list
+	//shoppingList.innerHTML += `<li>${inputValue}</li>`; // Adds input to list
+	let itemKey = item[0]; // gets the key of shoppingInDB
+	let itemValue = item[1]; // gets the value of shoppingInDB
+
+	let appendItem = document.createElement("li"); // creating the list element to be inserted into the HTML
+	appendItem.textContent = itemValue; // updates the text content which will be added into the list
+	shoppingList.append(appendItem); // appending the value as a list to the HTML
+
+	appendItem.addEventListener("click", function () {
+		// function to remove item by double clicking
+		let removeItem = ref(database, `Shopping/${itemKey}`); // new Var this links to the exact location to remove the item
+		remove(removeItem); // remove firebase function which actions the removeItem variable
+	});
 }
 
 //IMPORTANT need to clear list if changes are made in DB directly so it will re populate, this will not duplicate data now
